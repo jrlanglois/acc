ac3bc6c6dc4c95e342db5b638a3b8219a6f65091
Joël R. Langlois <joel.r.langlois@gmail.com>
2015-07-11 15:19:33 -0400

    Added JUCE module information file

--- Source/ACC/ACCModule.h
+++ Source/ACC/ACCModule.h
@@ -3,6 +3,20 @@
 
 #include "JuceHeader.h"
 
+//=============================================================================
+/** Config: DONT_SET_USING_ACC_NAMESPACE
+
+    If your uses a lot of ACC classes, declaring
+    using namespace acc obviously will save you a lot of typing!
+
+    You may disable this default by defining DONT_SET_USING_ACC_NAMESPACE somewhere,
+    and setting it to 1.
+*/
+#ifndef DONT_SET_USING_ACC_NAMESPACE
+ #define DONT_SET_USING_ACC_NAMESPACE 0
+#endif
+
+//=============================================================================
 namespace acc
 {
     #include "core/ACCCodeTokeniser.h"
@@ -13,6 +27,9 @@ namespace acc
     #include "graphics/ACCWindow.h"
 }
 
-using namespace acc;
+//=============================================================================
+#if ! DONT_SET_USING_ACC_NAMESPACE
+ using namespace acc;
+#endif
 
 #endif //ACC_MODULE_H
