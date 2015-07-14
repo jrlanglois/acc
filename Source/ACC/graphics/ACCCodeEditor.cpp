f10591e09649b9e56f7c5e36c773c47cbacfa810
Joël R. Langlois <joel.r.langlois@gmail.com>
2015-07-13 21:46:26 -0400

    Added a default preference

--- Source/ACC/graphics/ACCCodeEditor.cpp
+++ Source/ACC/graphics/ACCCodeEditor.cpp
@@ -3,4 +3,5 @@ static ACCCodeTokeniser accCodeTokeniser;
 ACCCodeEditorComponent::ACCCodeEditorComponent (CodeDocument& codeDocument) :
     CodeEditorComponent (codeDocument, &accCodeTokeniser)
 {
+    setTabSize (4, true);
 }
