820d1f02a188207b1e776795b20fc8d54dbb4dbf
Joël R. Langlois <joel.r.langlois@gmail.com>
2015-07-13 21:28:20 -0400

    Finally displaying a code editor, with a default file loaded

--- Source/ACC/graphics/ACCMainComponent.cpp
+++ Source/ACC/graphics/ACCMainComponent.cpp
@@ -1,5 +1,48 @@
-ACCMainComponent::ACCMainComponent()
+class ACCToolbarItemFactory : public ToolbarItemFactory
 {
+public:
+    ACCToolbarItemFactory()
+    {
+    }
+
+    void getAllToolbarItemIds (Array<int>& ids) override
+    {
+        getDefaultItemSet (ids);
+    }
+
+    void getDefaultItemSet (Array<int>& ids) override
+    {
+        ids.add (createNewFile);
+        ids.add (saveFile);
+        ids.add (openFile);
+        ids.add (separatorBarId);
+        ids.add (undo);
+        ids.add (redo);
+        ids.add (compile);
+    }
+
+    ToolbarItemComponent* createItem (int itemId) override
+    {
+        return nullptr;
+    }
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ACCToolbarItemFactory)
+};
+
+//==============================================================================
+ACCMainComponent::ACCMainComponent() :
+    menuBarComponent (nullptr)
+{
+    codeEditorComponent = new ACCCodeEditorComponent (codeDocument);
+
+    addAndMakeVisible (&menuBarComponent);
+    addAndMakeVisible (&toolbar);
+    addAndMakeVisible (codeEditorComponent);
+
+    setFile (File ("M:/Programming/jrl_acc/Source/original/zcommon.acs"));
+
     setSize (800, 600);
 }
 
@@ -10,15 +53,89 @@ Result ACCMainComponent::setFile (const File& file)
     if (result.failed())
         return result;
 
-    codeDocument.replaceAllContent (file.loadFileAsString());
-    component = new ACCCodeEditorComponent (codeDocument);
-    addAndMakeVisible (component);
+    codeEditorComponent->loadContent (file.loadFileAsString());
 
     return Result::ok();
 }
 
 void ACCMainComponent::resized()
 {
-    if (component != nullptr)
-        component->setBounds (getLocalBounds());
+    enum
+    {
+        barHeight = 32,
+        codeEditorOffset = barHeight * 2
+    };
+
+    const int w = getWidth();
+
+    menuBarComponent.setBounds (0, 0, w, barHeight);
+    toolbar.setBounds (0, barHeight, w, barHeight);
+
+    if (codeEditorComponent != nullptr)
+        codeEditorComponent->setBounds (0, codeEditorOffset, w, getHeight() - codeEditorOffset);
+}
+
+//==============================================================================
+StringArray ACCMainComponent::getMenuBarNames()
+{
+    StringArray names;
+
+    names.add (TRANS ("File"));
+    names.add (TRANS ("Edit"));
+    names.add (TRANS ("View"));
+    names.add (TRANS ("Build"));
+    names.add (TRANS ("Tools"));
+    names.add (TRANS ("Help"));
+
+    return names;
+}
+
+PopupMenu ACCMainComponent::getMenuForIndex (const int topLevelMenuIndex,
+                                             const String& menuName)
+{
+    PopupMenu popupMenu;
+
+    if (menuName == TRANS ("File"))
+    {
+
+    }
+    else if (menuName == TRANS ("Edit"))
+    {
+
+    }
+    else if (menuName == TRANS ("View"))
+    {
+
+    }
+    else if (menuName == TRANS ("Build"))
+    {
+
+    }
+    else if (menuName == TRANS ("Tools"))
+    {
+
+    }
+    else if (menuName == TRANS ("Help"))
+    {
+
+    }
+
+    return popupMenu;
+}
+
+void ACCMainComponent::menuItemSelected (const int menuItemID,
+                                         const int topLevelMenuIndex)
+{
+
+}
+
+void ACCMainComponent::menuBarItemsChanged (MenuBarModel* const menuBarModel)
+{
+
+}
+
+void ACCMainComponent::menuCommandInvoked (MenuBarModel* const menuBarModel,
+                                           const ApplicationCommandTarget::InvocationInfo& info)
+{
+
 }
--- Source/ACC/graphics/ACCMainComponent.h
+++ Source/ACC/graphics/ACCMainComponent.h
@@ -1,7 +1,9 @@
 #ifndef ACC_MAIN_COMPONENT_H
 #define ACC_MAIN_COMPONENT_H
 
-class ACCMainComponent : public Component
+class ACCMainComponent : public Component,
+                         private MenuBarModel,
+                         private MenuBarModel::Listener
 {
 public:
     ACCMainComponent();
@@ -12,11 +14,24 @@ public:
     //==============================================================================
     /** @internal */
     void resized() override;
+    /** @internal */
+    StringArray getMenuBarNames() override;
+    /** @internal */
+    PopupMenu getMenuForIndex (int topLevelMenuIndex, const String& menuName) override;
+    /** @internal */
+    void menuItemSelected (int menuItemID, int topLevelMenuIndex) override;
+    /** @internal */
+    void menuBarItemsChanged (MenuBarModel* menuBarModel) override;
+    /** @internal */
+    void menuCommandInvoked (MenuBarModel* menuBarModel, const ApplicationCommandTarget::InvocationInfo& info) override;
 
 private:
     //==============================================================================
+    MenuBarComponent menuBarComponent;
+    Toolbar toolbar;
+
     CodeDocument codeDocument;
-    ScopedPointer<ACCCodeEditorComponent> component;
+    ScopedPointer<ACCCodeEditorComponent> codeEditorComponent;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ACCMainComponent)
