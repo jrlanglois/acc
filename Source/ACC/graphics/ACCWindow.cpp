53a6f5bb3cf237ee413a896226d0dd60efdc63ee
Joël R. Langlois <joel.langlois@youi.tv>
2015-07-16 08:11:34 -0400

    Made the window resizable (since it apparently isn't by default), and limited the minimum window size to something sensible.
    If you really need it any smaller, don't be a dummy and minimise it!

--- Source/ACC/graphics/ACCWindow.cpp
+++ Source/ACC/graphics/ACCWindow.cpp
@@ -2,6 +2,11 @@ ACCWindow::ACCWindow() :
     DocumentWindow (ProjectInfo::projectName, Colours::lightgrey, DocumentWindow::allButtons)
 {
     setUsingNativeTitleBar (true);
+    setResizable (true, false);
+
+    ComponentBoundsConstrainer* constrainer = new ComponentBoundsConstrainer();
+    constrainer->setMinimumSize (500, 500); //Limit minimum window size to something sensible
+    setConstrainer (constrainer);
 
     setContentOwned (new ACCMainComponent(), true);
     centreWithSize (getWidth(), getHeight());
