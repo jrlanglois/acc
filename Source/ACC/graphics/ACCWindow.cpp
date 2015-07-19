72e4968656fc043d80164e8897a427a434058887
Joël R. Langlois <joel.r.langlois@gmail.com>
2015-07-19 19:58:43 -0400

    Removed silly memory leak

--- Source/ACC/graphics/ACCWindow.cpp
+++ Source/ACC/graphics/ACCWindow.cpp
@@ -4,13 +4,17 @@ ACCWindow::ACCWindow() :
     setUsingNativeTitleBar (true);
     setResizable (true, false);
 
-    ComponentBoundsConstrainer* constrainer = new ComponentBoundsConstrainer();
-    constrainer->setMinimumSize (500, 500); //Limit minimum window size to something sensible
-    setConstrainer (constrainer);
+    constrainer.setMinimumSize (500, 500); //Limit minimum window size to something sensible
+    setConstrainer (&constrainer);
 
     setContentOwned (new ACCMainComponent(), true);
     centreWithSize (getWidth(), getHeight());
     setVisible (true);
+}
+
+ACCWindow::~ACCWindow()
+{
+    setConstrainer (nullptr);
 }
 
 void ACCWindow::closeButtonPressed()
--- Source/ACC/graphics/ACCWindow.h
+++ Source/ACC/graphics/ACCWindow.h
@@ -5,12 +5,16 @@ class ACCWindow : public DocumentWindow
 {
 public:
     ACCWindow();
+    ~ACCWindow();
 
     //==============================================================================
     /** @internal */
     void closeButtonPressed() override;
 
 private:
+    //==============================================================================
+    ComponentBoundsConstrainer constrainer;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ACCWindow)
 };
