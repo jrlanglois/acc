dd5745201bbd56c06a5b8983efd110419b7f1ca2
Joël R. Langlois <joel.r.langlois@gmail.com>
2015-07-19 19:37:48 -0400

    Added dynamic font size changing in code editor component.
    Added new default colours to the code editor component.

--- Source/ACC/graphics/ACCCodeEditor.cpp
+++ Source/ACC/graphics/ACCCodeEditor.cpp
@@ -1,7 +1,37 @@
 static ACCCodeTokeniser accCodeTokeniser;
 
 ACCCodeEditorComponent::ACCCodeEditorComponent (CodeDocument& codeDocument) :
-    CodeEditorComponent (codeDocument, &accCodeTokeniser)
+    CodeEditorComponent (codeDocument, &accCodeTokeniser),
+    fontSize (12.0f)
 {
     setTabSize (4, true);
+
+    setFont (getFont().withHeight (fontSize));
+
+    LookAndFeel& laf = getLookAndFeel();
+    laf.setColour (CodeEditorComponent::highlightColourId, Colour (234, 234, 242));
+    laf.setColour (CodeEditorComponent::lineNumberBackgroundId, Colours::white);
+    laf.setColour (CodeEditorComponent::lineNumberTextId, Colour (43, 145, 175));
+}
+
+//==============================================================================
+void ACCCodeEditorComponent::mouseWheelMove (const MouseEvent& e, const MouseWheelDetails& mwd)
+{
+    if (e.mods.isCommandDown())
+    {
+        const float lastSize = fontSize;
+        const float scrollRatio = 1.2f;
+
+        if (mwd.deltaY > 0.0f)
+            fontSize += scrollRatio;
+        else if (mwd.deltaY < 0.0f)
+            fontSize -= scrollRatio;
+
+        if (fontSize != lastSize)
+            setFont (getFont().withHeight (fontSize));
+    }
+    else
+    {
+        CodeEditorComponent::mouseWheelMove (e, mwd);
+    }
 }
--- Source/ACC/graphics/ACCCodeEditor.h
+++ Source/ACC/graphics/ACCCodeEditor.h
@@ -6,7 +6,14 @@ class ACCCodeEditorComponent : public CodeEditorComponent
 public:
     ACCCodeEditorComponent (CodeDocument& codeDocument);
 
+    //==============================================================================
+    /** @internal */
+    void mouseWheelMove (const MouseEvent&, const MouseWheelDetails&) override;
+
 private:
+    //==============================================================================
+    float fontSize;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ACCCodeEditorComponent)
 };
